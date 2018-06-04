aa838adf21bc51c5a0c76f3a0613df5d40b55fb8
suncyly <ly1582840@163.com>
2018-06-04 16:09:10 +0800

    修改测试

--- Mycat-Server-1.6/src/main/java/io/mycat/MycatStartup.java
+++ Mycat-Server-1.6/src/main/java/io/mycat/MycatStartup.java
@@ -55,7 +55,7 @@ public final class MycatStartup {
 
             // startup
             server.startup();
-            System.out.println("MyCAT Server startup successfully. see logs in logs/mycat.log");
+            System.out.println("MyCATtt Server startup successfully. see logs in logs/mycat.log");
 
         } catch (Exception e) {
             SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
